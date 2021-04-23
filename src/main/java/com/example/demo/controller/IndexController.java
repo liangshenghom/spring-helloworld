b60a77b4e2c2ac7e35fbea663f290468bdf14f27
liangshenghom <793457851@qq.com>
2021-04-23 15:45:33 +0800

    88

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 	
-	@GetMapping("/")
+	@GetMapping("/index")
 	public String  index() {
 		
 		return "首页";
